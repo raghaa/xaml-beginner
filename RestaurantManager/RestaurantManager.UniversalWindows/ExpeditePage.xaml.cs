bd5976c1101cf530b5e77d0e5110ce6f53fdb1bd
raghaa <raghavendran@live.in>
2015-10-11 16:54:50 +0530

    added ui navigation
    buttons added with page navigation

---
+++ RestaurantManager/RestaurantManager.UniversalWindows/ExpeditePage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace RestaurantManager.UniversalWindows
+{
+    public sealed partial class ExpeditePage : Page
+    {
+        public ExpeditePage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ RestaurantManager/RestaurantManager.UniversalWindows/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace RestaurantManager.UniversalWindows
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void ExpeditePage_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(ExpeditePage));
+        }
+    }
+}
